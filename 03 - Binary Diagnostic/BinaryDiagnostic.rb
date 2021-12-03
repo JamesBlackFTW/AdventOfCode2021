7e1c95dc6ce197c168fec358a65c7662162a0712
James Black <jamesblacksoftware@gmail.com>
2021-12-03 15:13:11 +0000

    Array manipulation changes on 3.2
    Clean code is best code

--- 03 - Binary Diagnostic/BinaryDiagnostic.rb
+++ 03 - Binary Diagnostic/BinaryDiagnostic.rb
@@ -23,60 +23,54 @@ class BinaryDiagnosticMachine
   def solve_advanced_diagnostic
     oxygen = solve_oxygen_diagnostic
     carbon = solve_carbon_diagnostic
+
     p oxygen * carbon
   end
 
   private def solve_oxygen_diagnostic
-    binaryString = @input
-
-    (0..@input.count).each { |i|
-      mostPopular = solve_most_popular_bit(binaryString, i)
-      binaryString = binaryString.select {|val| val[i] == mostPopular }
-      if binaryString.length == 1
-        return binaryString[0].join.to_i(2)
-      end
-    }
-  end
-
-  private def solve_carbon_diagnostic
-    binaryString = @input
-
-    (0..@input.count).each { |i|
-      leastPopular = solve_least_popular_bit(binaryString, i)
-      binaryString = binaryString.select {|val| val[i] == leastPopular }
-      if binaryString.length == 1
-        return binaryString[0].join.to_i(2)
-      end
-    }
-  end
-
-  private def solve_most_popular_bit(input, index)
-    zeroCount = 0
-    input.each do |value|
-      if value[index] == "0"
-        zeroCount = zeroCount + 1
+    array = @input.clone
+    @input.size.times do |i|
+      filterVal = solve_most_popular_bit(array, i)
+      array.select! {|val| val[i] == filterVal }
+      if array.length == 1
+        break
       end
     end
 
-    if zeroCount <= (input.count / 2)
-      return '1'
+    return array[0].join.to_i(2)
+  end
+
+  private def solve_carbon_diagnostic
+    array = @input.clone
+    @input.size.times do |i|
+      filterVal = solve_least_popular_bit(array, i)
+      array.select! {|val| val[i] == filterVal }
+
+      if array.length == 1
+        break
+      end
+    end
+
+    return array[0].join.to_i(2)
+  end
+
+  private def solve_most_popular_bit(input, index)
+    set = input.transpose[index]
+    zeros = set.select.count {|x| x == "0"}
+    if zeros > (set.count / 2)
+      "0"
     else
-      return '0'
+      "1"
     end
   end
 
   private def solve_least_popular_bit(input, index)
-    zeroCount = 0
-    input.each do |value|
-      if value[index] == "0"
-        zeroCount = zeroCount + 1
-      end
-    end
-
-    if zeroCount > (input.count / 2)
-      return '1'
+    set = input.transpose[index]
+    zeros = set.select.count {|x| x == "0"}
+    if zeros <= (set.count / 2)
+      "0"
     else
-      return '0'
+      "1"
     end
   end
 end
